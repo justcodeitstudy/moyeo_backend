2c27c0dbf2854634e254699bbc151241e3f14bfc
jjoylee <ekdnlt1995@gmail.com>
2022-10-05 20:13:20 +0900

    테스트 코드 개발중

---
+++ src/test/java/com/justcodeit/moyeo/study/interfaces/resource/UserControllerTest.java
@@ -0,0 +1,86 @@
+package com.justcodeit.moyeo.study.interfaces.resource;
+import com.justcodeit.moyeo.study.application.user.UserService;
+import com.justcodeit.moyeo.study.application.user.dto.GetUserResDto;
+import com.justcodeit.moyeo.study.persistence.User;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willReturn;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles(profiles = "local")
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+
+    @DisplayName("[API][GET] 게시글 리스트 조회")
+    @Test
+    void testGetAllContents() throws Exception {
+        //given
+        User user = new User("username","email","picture", User.Role.USER);
+        given(userService.accessProfile("userId"))
+        .willReturn(new GetUserResDto(
+                "userId",
+                "email",
+                user.getNickname(),
+                user.getIntroduction(),
+                user.getJob1(),
+                user.getJob2(),
+                user.getJob3(),
+                user.getPortfolio(),
+                user.getSkills()
+        ));
+
+        //when & then
+        mockMvc.perform(get("/users/userId")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
+    }
+}
