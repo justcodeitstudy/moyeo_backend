b073f95359be9a8d705713f4f9a9bcec2370fe6e
jjoylee <ekdnlt1995@gmail.com>
2022-10-10 21:52:24 +0900

    usertoken에 userId 추가

--- src/main/java/com/justcodeit/moyeo/study/model/jwt/AuthTokenConverter.java
+++ src/main/java/com/justcodeit/moyeo/study/model/jwt/AuthTokenConverter.java
@@ -14,16 +14,18 @@ public class AuthTokenConverter {
 
   public UserToken fromToken(String t) {
     var claims = jwt.claims(t);
+    var userId = claims.get("userId", String.class);
     var email = claims.get("email", String.class);
     var displayName = claims.get("displayName", String.class);
     var role = claims.get("role", String.class);
-    return new UserToken(email, displayName, role);
+    return new UserToken(userId, email, displayName, role);
     // todo 토큰이 변경되면 같이 수정되어야 함
   }
 
   public String toTokenString(Authentication auth) {
     final var principal = (UserPrincipal) auth.getPrincipal();
     Map<String, Object> claims = new HashMap<>() {{
+      put("userId", principal.getUserId());
       put("email", principal.getEmail());
       put("role", principal.getRoleType());
       put("displayName", principal.getDisplayName());
