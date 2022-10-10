10e89a27383dd00e05c6e0e233ca6c52f903ba1c
jjoylee <ekdnlt1995@gmail.com>
2022-10-10 23:02:43 +0900

    리뷰반영. userId 주입으로 변경.

--- src/main/java/com/justcodeit/moyeo/study/model/OAuthInfoToUserConverter.java
+++ src/main/java/com/justcodeit/moyeo/study/model/OAuthInfoToUserConverter.java
@@ -4,6 +4,7 @@ import com.justcodeit.moyeo.study.common.RandomIdUtil;
 import com.justcodeit.moyeo.study.model.oauth.ProviderUserInfo;
 import com.justcodeit.moyeo.study.model.type.Role;
 import com.justcodeit.moyeo.study.persistence.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +12,7 @@ public class OAuthInfoToUserConverter implements ModelConverter<ProviderUserInfo
 
   @Override
   public User convert(ProviderUserInfo s) {
-    return new User(new RandomIdUtil().userId(), s.getEmail(), s.getPicture(), Role.USER, s.getName(), s.getProviderType(),
+    return new User(s.getUserId(), s.getEmail(), s.getPicture(), Role.USER, s.getName(), s.getProviderType(),
         s.getDomesticId());
   }
 }
