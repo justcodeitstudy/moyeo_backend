d2f8712f42c496a0e2c2678f365a48728d80f833
beetrbgus <devbeetrb0214@gmail.com>
2022-10-11 03:14:26 +0900

    Skill 조회 및 등록 관련
    - 단일 등록 추가 및 전체 등록 url 수정
    - 단일 조회 및 목록 조회 추가

--- src/main/java/com/justcodeit/moyeo/study/interfaces/resource/SkillController.java
+++ src/main/java/com/justcodeit/moyeo/study/interfaces/resource/SkillController.java
@@ -1,11 +1,20 @@
 package com.justcodeit.moyeo.study.interfaces.resource;
 
 import com.justcodeit.moyeo.study.application.skill.SkillService;
+import com.justcodeit.moyeo.study.interfaces.dto.skill.SkillCreateDto;
+import com.justcodeit.moyeo.study.model.skill.SkillDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.List;
 
 @RequestMapping("/skill")
 @RestController
@@ -14,7 +23,24 @@ public class SkillController {
     private final SkillService skillService;
 
     @PostMapping
+    public ResponseEntity<SkillDto> createSkill(@ModelAttribute("skillCreateDto") @Valid SkillCreateDto skillCreateDto) throws IOException {
+        if(!skillCreateDto.isSvg()) {
+            throw new IOException();
+        }
+        return ResponseEntity.ok(skillService.saveSkill(skillCreateDto));
+    }
+    @GetMapping
+    public ResponseEntity<SkillDto> findSkillByName(@RequestParam(name = "name") String name) {
+        return ResponseEntity.ok(skillService.findSkillByName(name));
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<SkillDto>> findAllSkill() {
+        return ResponseEntity.ok(skillService.findSkillAll());
+    }
+
+    @PostMapping("/upload")
     public ResponseEntity<Boolean> uploadZip() {
-        return ResponseEntity.ok(skillService.skillSaveAllFromS3());
+        return ResponseEntity.ok(skillService.skillcreateAllFromS3());
     }
 }
