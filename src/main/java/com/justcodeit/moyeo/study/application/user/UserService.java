83a266f76c1f51d61ed715742be0574d8d91f40f
jjoylee <ekdnlt1995@gmail.com>
2022-10-13 21:02:16 +0900

    readonly 조건 추가

--- src/main/java/com/justcodeit/moyeo/study/application/user/UserService.java
+++ src/main/java/com/justcodeit/moyeo/study/application/user/UserService.java
@@ -6,6 +6,7 @@ import com.justcodeit.moyeo.study.persistence.User;
 import com.justcodeit.moyeo.study.persistence.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,6 +14,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public GetUserResDto accessProfile(String userId) {
         var user = getUser(userId);
         return new GetUserResDto(
