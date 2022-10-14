07f40d4b21ff8e273f80e9b493c73443ef7d6d22
jjoylee <ekdnlt1995@gmail.com>
2022-10-14 14:44:26 +0900

    테스트 수정, userskillservice 테스트 추가

--- src/test/java/com/justcodeit/moyeo/study/application/user/UserServiceTest.java
+++ src/test/java/com/justcodeit/moyeo/study/application/user/UserServiceTest.java
@@ -4,30 +4,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.justcodeit.moyeo.study.application.user.exception.UserCannotFoundException;
 import com.justcodeit.moyeo.study.model.type.Role;
 import com.justcodeit.moyeo.study.persistence.User;
-import com.justcodeit.moyeo.study.persistence.UserSkill;
 import com.justcodeit.moyeo.study.persistence.repository.UserRepository;
+import java.util.List;
 import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
+    @InjectMocks
     UserService userService;
+    @Mock
     UserRepository userRepository;
+    @Mock
+    UserSkillService userSkillService;
     User testUser;
-
-    @BeforeEach
-    public void setup() {
-        userRepository = mock(UserRepository.class);
-        userService = new UserService(userRepository);
-    }
 
     @Test
     void userId_invalid_throw_exception() {
@@ -40,10 +42,9 @@ class UserServiceTest {
     void userId_valid_return_dto() {
         when(userRepository.findByUserId(anyString())).thenAnswer((e) -> {
             testUser = new User((String)e.getArgument(0), "email", "picture", Role.USER, "display", "provider", "domesticId", "nickname");
-            testUser.addUserSkill(new UserSkill(testUser, 1l));
-            testUser.addUserSkill(new UserSkill(testUser, 2l));
             return Optional.of(testUser);
         });
+        when(userSkillService.getSkillIds(any())).thenReturn(List.of(1L,2L));
         var userId = "testUserId";
         var result = userService.accessProfile(userId);
         assertEquals(testUser.getEmail(), result.getEmail());
