d35ecd1451783dd9e3701bf47fde50585744d58f
jjoylee <ekdnlt1995@gmail.com>
2022-10-17 21:33:52 +0900

    리뷰 반영

--- src/main/java/com/justcodeit/moyeo/study/persistence/repository/UserSkillRepository.java
+++ src/main/java/com/justcodeit/moyeo/study/persistence/repository/UserSkillRepository.java
@@ -12,7 +12,7 @@ public interface UserSkillRepository extends JpaRepository<UserSkill, Long> {
     @Query("SELECT us.skillId FROM UserSkill us WHERE us.userId = :userId ORDER BY us.id")
     List<Long> findSkillIdsByUserId(@Param("userId") Long userId);
 
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Modifying(clearAutomatically = true)
     @Query("DELETE FROM UserSkill us WHERE us.userId = :userId")
     void deleteByUserId(@Param("userId") Long userId);
 }
