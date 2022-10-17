b0f929b941a2158b22fec691693836f5b84be86f
jjoylee <ekdnlt1995@gmail.com>
2022-10-17 21:12:21 +0900

    프로필 수정 api 개발

--- src/main/java/com/justcodeit/moyeo/study/persistence/repository/SkillRepository.java
+++ src/main/java/com/justcodeit/moyeo/study/persistence/repository/SkillRepository.java
@@ -1,8 +1,15 @@
 package com.justcodeit.moyeo.study.persistence.repository;
 
 import com.justcodeit.moyeo.study.persistence.Skill;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SkillRepository extends JpaRepository<Skill, Long> {
+
     Skill findByName(String name);
+
+    @Query("SELECT COUNT(s) FROM Skill s WHERE s.id IN (:ids)")
+    Long getCountByIds(@Param("ids") List<Long> ids);
 }
--- src/main/java/com/justcodeit/moyeo/study/persistence/repository/UserSkillRepository.java
+++ src/main/java/com/justcodeit/moyeo/study/persistence/repository/UserSkillRepository.java
@@ -3,10 +3,16 @@ package com.justcodeit.moyeo.study.persistence.repository;
 import com.justcodeit.moyeo.study.persistence.UserSkill;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserSkillRepository extends JpaRepository<UserSkill, Long> {
-    @Query("select us.skillId from UserSkill us where us.userId = :userId")
+
+    @Query("SELECT us.skillId FROM UserSkill us WHERE us.userId = :userId ORDER BY us.id")
     List<Long> findSkillIdsByUserId(@Param("userId") Long userId);
+
+    @Modifying
+    @Query("DELETE FROM UserSkill us WHERE us.userId = :userId")
+    void deleteByUserId(@Param("userId") Long userId);
 }
