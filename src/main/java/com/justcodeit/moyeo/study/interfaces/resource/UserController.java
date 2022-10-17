c8771268f1800499d408e9c54a57c4a98f789413
jjoylee <ekdnlt1995@gmail.com>
2022-10-17 14:32:58 +0900

    프로필 수정 api 개발

--- src/main/java/com/justcodeit/moyeo/study/interfaces/resource/UserController.java
+++ src/main/java/com/justcodeit/moyeo/study/interfaces/resource/UserController.java
@@ -1,16 +1,20 @@
 package com.justcodeit.moyeo.study.interfaces.resource;
 
 import com.justcodeit.moyeo.study.application.user.UserService;
+import com.justcodeit.moyeo.study.interfaces.dto.user.EditProfileReqDto;
 import com.justcodeit.moyeo.study.model.jwt.UserToken;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,8 +36,19 @@ public class UserController {
     @Parameter(name = "X-MOYEO-AUTH-TOKEN", in = ParameterIn.HEADER, required = true)
     @Secured("ROLE_USER")
     @GetMapping("/me")
-    public ResponseEntity getProfile(@Parameter(hidden=true) @AuthenticationPrincipal UserToken userToken) {
+    public ResponseEntity getProfile(
+        @Parameter(hidden = true) @AuthenticationPrincipal UserToken userToken) {
         var getUserResDto = userService.accessProfile(userToken.getUserId());
         return ResponseEntity.ok(getUserResDto);
+    }
+
+    @Operation(summary = "내 프로필 수정 ", description = "요청시 사용된 jwt가 유효한지 확인 및 프로필 수정")
+    @Parameter(name = "X-MOYEO-AUTH-TOKEN", in = ParameterIn.HEADER, required = true)
+    @Secured("ROLE_USER")
+    @PatchMapping("/me")
+    public ResponseEntity<Void> editProfile(@AuthenticationPrincipal UserToken userToken,
+        @RequestBody @Valid EditProfileReqDto editProfileReqDto) {
+        userService.editProfile(userToken.getUserId(), editProfileReqDto);
+        return ResponseEntity.ok(null);
     }
 }
