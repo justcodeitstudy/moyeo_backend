e446209d5e19751c805364ba5a1c7655e82a30ec
yunhozz <qkrdbsgh1121@naver.com>
2022-10-25 16:47:14 +0900

    style: import 수정

--- src/main/java/com/justcodeit/moyeo/study/persistence/repository/querydsl/PostCustomRepositoryImpl.java
+++ src/main/java/com/justcodeit/moyeo/study/persistence/repository/querydsl/PostCustomRepositoryImpl.java
@@ -27,11 +27,11 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static com.justcodeit.moyeo.study.persistence.QPost.*;
-import static com.justcodeit.moyeo.study.persistence.QPostSkill.*;
-import static com.justcodeit.moyeo.study.persistence.QRecruitment.*;
-import static com.justcodeit.moyeo.study.persistence.QScrap.*;
-import static com.justcodeit.moyeo.study.persistence.QSkill.*;
+import static com.justcodeit.moyeo.study.persistence.QPost.post;
+import static com.justcodeit.moyeo.study.persistence.QPostSkill.postSkill;
+import static com.justcodeit.moyeo.study.persistence.QRecruitment.recruitment;
+import static com.justcodeit.moyeo.study.persistence.QScrap.scrap;
+import static com.justcodeit.moyeo.study.persistence.QSkill.skill;
 
 @Repository
 @RequiredArgsConstructor
