00fc8fe483171bbbe6a498ea8d850dd774f3bbee
beetrbgus <devbeetrb0214@gmail.com>
2022-10-28 22:52:28 +0900

    hotfix 자신 작성한 모집글 리스트 조회 누락된 부분 추가
    - List 임포트 빠짐.

--- src/main/java/com/justcodeit/moyeo/study/interfaces/resource/PostController.java
+++ src/main/java/com/justcodeit/moyeo/study/interfaces/resource/PostController.java
@@ -37,6 +37,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Tag(name = "모집글", description = "모집글 CRUD API.")
 @RestController
