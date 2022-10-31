ba2bed4c699e4592f47001709dab0016907e31a2
beetrbgus <devbeetrb0214@gmail.com>
2022-10-31 21:52:11 +0900

    SKill OpenApi 수정
    - Operation 추가
    - 스키마 처리
    - 단건 조회시 Service 단에서 ElseThrow 처리할수 있게 변경

--- src/main/java/com/justcodeit/moyeo/study/persistence/repository/SkillRepository.java
+++ src/main/java/com/justcodeit/moyeo/study/persistence/repository/SkillRepository.java
@@ -2,10 +2,12 @@ package com.justcodeit.moyeo.study.persistence.repository;
 
 import com.justcodeit.moyeo.study.persistence.Skill;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SkillRepository extends JpaRepository<Skill, Long> {
-    Skill findByName(String name);
+    Optional<Skill> findByName(String name);
 
     long countByIdIn(List ids);
 }
