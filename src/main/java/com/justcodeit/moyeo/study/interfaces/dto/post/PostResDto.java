466f648c008a933718424ff3779e4f450f7aaa84
beetrbgus <devbeetrb0214@gmail.com>
2022-11-23 05:46:59 +0900

    Post /get/{id} modify
    - 모집글 상세 내용 추가 + 닉네임 추가
    - Mapper에 ignore 추가

--- src/main/java/com/justcodeit/moyeo/study/interfaces/dto/post/PostResDto.java
+++ src/main/java/com/justcodeit/moyeo/study/interfaces/dto/post/PostResDto.java
@@ -1,8 +1,7 @@
 package com.justcodeit.moyeo.study.interfaces.dto.post;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.justcodeit.moyeo.study.interfaces.dto.postskill.PostSkillResDto;
 import com.justcodeit.moyeo.study.interfaces.dto.recruitment.RecruitmentDto;
+import com.justcodeit.moyeo.study.model.post.ProgressType;
 import com.justcodeit.moyeo.study.model.post.RecruitStatus;
 import com.justcodeit.moyeo.study.model.post.PostType;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -16,15 +15,28 @@ import java.util.List;
 @Setter
 @Getter
 public class PostResDto {
-    @Schema(description = "모집글 번호")
+    @Schema(description = "글 번호")
     private Long id;
-    @Schema(description = "모집글 번호")
+    @Schema(description = "제목")
     private String title;
-    @Schema(description = "모집글 번호")
+    @Schema(description = "내용")
     private String content;
+    @Schema(description = "userId")
+    private String userId;
+    @Schema(description = "닉네임")
+    private String userNick;
+    @Schema(description = "진행방식")
+    private ProgressType progressType;
+    @Schema(description = "조회수")
+    private long viewCount;
+    @Schema(description = "종류")
     private PostType postType;
-    private RecruitStatus recruitStatus;
+    @Schema(description = "모집 상태")
+    private RecruitStatus recruitStatus; // 모집상태
+    @Schema(description = "모집분류 목록")
     private List<RecruitmentDto> recruitmentList;
+    @Schema(description = "기술스택 목록")
     private List<Long> skillIds;
+    @Schema(description = "생성일")
     private LocalDateTime createdAt;
 }
